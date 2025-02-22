2130acd039344092d8257b7648732b4245a53276
Aurora Padilla <123429652+auro-fre34@users.noreply.github.com>
2025-02-22 12:06:37 -0600

    CRUD de category
    cruds de category
    get all
    get all by id
    add
    update
    delete

---
+++ src/main/java/com/project/demo/rest/category/CategoryController.java
@@ -0,0 +1,108 @@
+package com.project.demo.rest.category;
+
+import com.project.demo.logic.entity.category.Category;
+import com.project.demo.logic.entity.category.CategoryRepositoy;
+import com.project.demo.logic.entity.http.GlobalResponseHandler;
+import com.project.demo.logic.entity.http.Meta;
+import com.project.demo.logic.entity.user.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+  @Autowired
+  private CategoryRepositoy categoryRepositoy;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  //GET -POST - PATCH - PUT - DELETE
+
+      @GetMapping
+      public ResponseEntity<?> getCategory(
+          @RequestParam(defaultValue = "1") int page,
+          @RequestParam(defaultValue = "10") int size,
+          HttpServletRequest request
+      ) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Category> categoryPage = categoryRepositoy.findAll(pageable);
+        Meta meta = new Meta(request.getMethod(), request.getRequestURL().toString());
+        meta.setTotalPages(categoryPage.getTotalPages());
+        meta.setTotalElements(categoryPage.getTotalElements());
+        meta.setPageNumber(categoryPage.getNumber() + 1);
+        meta.setPageSize(categoryPage.getSize());
+
+        return new GlobalResponseHandler().handleResponse("Category retrieved successfully",
+            categoryPage.getContent(), HttpStatus.OK, meta);
+      }
+
+      @GetMapping("/{Id}")
+      @PreAuthorize("isAuthenticated()")
+      public ResponseEntity<?> getCetgoryById(@PathVariable Long Id, HttpServletRequest request) {
+        Optional<Category> foundCategory = categoryRepositoy.findById(Id);
+        if(foundCategory.isPresent())  {
+          return new GlobalResponseHandler().handleResponse(
+              "Category retrieved successfully",
+              foundCategory.get(),
+              HttpStatus.OK,
+              request);
+        } else {
+          return new GlobalResponseHandler().handleResponse(
+              "Category with id " + Id + " not found",
+              HttpStatus.NOT_FOUND,
+              request);
+        }
+      }
+
+      @PostMapping
+      @PreAuthorize("isAuthenticated() && hasAnyRole('SUPER_ADMIN')")
+      public ResponseEntity<?> addCategory(@RequestBody Category category, HttpServletRequest request) {
+        Category savedCategory = categoryRepositoy.save(category);
+        return new GlobalResponseHandler().handleResponse(
+            "Product successfully saved",
+            savedCategory,
+            HttpStatus.OK,
+            request);
+      }
+
+      @PutMapping
+      @PreAuthorize("isAuthenticated() && hasAnyRole('SUPER_ADMIN')")
+      public ResponseEntity<?> updateCategory(@RequestBody Category category, HttpServletRequest request) {
+        Category savedCategory = categoryRepositoy.save(category);
+        return new GlobalResponseHandler().handleResponse(
+            "Category successfully updated",
+            savedCategory,
+            HttpStatus.OK,
+            request);
+      }
+
+      @DeleteMapping("/{Id}")
+      @PreAuthorize("isAuthenticated() && hasAnyRole('SUPER_ADMIN')")
+      public ResponseEntity<?> deleteCategory (@PathVariable Long Id, HttpServletRequest request) {
+        Optional<Category> foundCategory = categoryRepositoy.findById(Id);
+        if(foundCategory.isPresent()) {
+          categoryRepositoy.deleteById(Id);
+          return new GlobalResponseHandler().handleResponse(
+              "Category successfully deleted",
+              HttpStatus.OK,
+              request);
+        } else {
+          return new GlobalResponseHandler().handleResponse(
+              "Category with id " + Id + " not found",
+              HttpStatus.NOT_FOUND,
+              request);
+        }
+      }
+}
